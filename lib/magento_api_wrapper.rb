7b00f963d5956ff5fa2fb0a1f8c6a41e796f872b
Anton Pershakov <main24.fc@gmail.com>
2014-03-23 01:09:15 +0600

    Simplify requiring of files from subdirectories

--- lib/magento_api_wrapper.rb
+++ lib/magento_api_wrapper.rb
@@ -4,27 +4,21 @@ require 'magento_api_wrapper/connection'
 require 'magento_api_wrapper/savon_client'
 
 require 'magento_api_wrapper/request'
-require 'magento_api_wrapper/requests/login'
-require 'magento_api_wrapper/requests/sales_order_list'
-require 'magento_api_wrapper/requests/sales_order_info'
-require 'magento_api_wrapper/requests/create_invoice'
-require 'magento_api_wrapper/requests/invoice_list'
-require 'magento_api_wrapper/requests/invoice_info'
-require 'magento_api_wrapper/requests/create_shipment'
-require 'magento_api_wrapper/requests/shipment_list'
-require 'magento_api_wrapper/requests/shipment_info'
-require 'magento_api_wrapper/requests/add_tracking_to_shipment'
+Dir[File.dirname(__FILE__) + "/magento_api_wrapper/requests/*.rb"].each do |file|
+  require file
+end
 
 require 'magento_api_wrapper/response'
 require 'magento_api_wrapper/responses'
 require 'magento_api_wrapper/version'
 
 require 'magento_api_wrapper/api'
-require 'magento_api_wrapper/api/sales'
-require 'magento_api_wrapper/api/cart'
-require 'magento_api_wrapper/api/catalog'
-require 'magento_api_wrapper/api/customer'
+Dir[File.dirname(__FILE__) + "/magento_api_wrapper/api/*.rb"].each do |file|
+  require file
+end
+
 require 'magento_api_wrapper/api_error'
+
 
 module MagentoApiWrapper
   class Error < StandardError; end
